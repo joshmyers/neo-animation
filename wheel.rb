e16856cdc911a6739c443319f92e4afcd5418e33
Nathan Witmer <nathan@zerowidth.com>
2014-12-20 23:36:49 -0700

    Separate color wheel script

--- wheel.rb
+++ wheel.rb
@@ -1,7 +1,6 @@
 require_relative "light_show"
 
 PIXELS = 24
-
 renderers = []
 
 require_relative "light_show/console_renderer"
@@ -12,33 +11,15 @@ if `uname -a` =~ /armv6l/ # raspberry pi
   neopixel = LightShow::NeopixelRenderer.new \
     :led_count      => PIXELS,
     :led_pin        => 18,
-    :led_brightness => 31,
+    :led_brightness => 63,
     :offset         => 8 # for sideways, 14 for upright
   renderers << neopixel
 end
 
 main = LightShow::Animation.new do |a|
   a.forever
-
-  # Color wipe animations
-  a.delay_ms 75
-  # a << LightShow::ColorWipe.new(1, 0, 0) # red color wipe
-  # a << LightShow::ColorWipe.new(0, 1, 0) # green color wipe
-  # a << LightShow::ColorWipe.new(0, 0, 1) # blue color wipe
-
-  # Theater chase animations
-  a.delay_ms 100
-  # a << LightShow::SolidColor.new(0, 0, 0)
-  # a << LightShow::TheaterChase.new(1, 1, 1)
-  # a << LightShow::TheaterChase.new(1, 0, 0)
-  # a << LightShow::TheaterChase.new(0, 0, 1)
-
   a.delay_ms 20
-  # a << LightShow::Rainbow.new
-  # a << LightShow::RainbowCycle.new
-
-  a.delay_ms 75
-  a << LightShow::TheaterChaseRainbow.new
+  a << LightShow::RainbowCycle.new
 end
 
 runner = LightShow::Interrupt.new(main) do |shutdown|
@@ -47,9 +28,7 @@ runner = LightShow::Interrupt.new(main) do |shutdown|
 end
 
 black = LightShow.color_frame(PIXELS, 0,0,0)
-
 trap("INT") { runner.stop! }
-
 runner.each_frame(black) do |frame|
   renderers.each { |renderer| renderer.render frame }
 end
