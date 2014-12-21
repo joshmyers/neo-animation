9122570474e4b5a2e784e193d8a03c6ade52decf
Nathan Witmer <nathan@zerowidth.com>
2014-12-20 20:00:06 -0700

    Combining and iteration based on previous frames

--- enum.rb
+++ enum.rb
@@ -1,25 +1,25 @@
-# NEXT: combining, and iteration based on previous frames.
+# A demonstration/exploration of generating animation "frames" by combining
+# objects that emit sequences of frames, with the following goals:
+#
+# * Declarative syntax for convenience and readability
+# * Composability, for easily assembling larger animations from smaller pieces
+# * Interruptability (see Flagged), so an animation sequence can be interrupted
+# * Lazily-evaluated combinations so clock-based animations can be mixed.
 
+# A top-level animation.
 class Animation
+
+  # Initialize a new animation. Yields the new instance for configuration.
   def initialize
     @animations = []
-    @loop = nil
-    @delay_ms = 0
+    @loop       = nil
+    @delay_ms   = 0
     yield self
   end
 
-  def delay_ms(ms)
-    @delay_ms = ms
-  end
-
-  def forever
-    raise ArgumentError, "there's only one forever!" if @loop
-    @forever = true
-    @loop = []
-  end
-
+  # Append a lower-level animation to this animation.
   def <<(anim)
-    anim = Delayed.new(@delay_ms, anim) if @delay_ms && @delay_ms > 0
+    anim = WithDelay.new(@delay_ms, anim) if @delay_ms && @delay_ms > 0
     if @forever
       @loop << anim
     else
@@ -27,53 +27,81 @@ class Animation
     end
   end
 
-  def each(&block)
-    iterate_frames @animations, &block
+  # Set the delay in milliseconds for subsequent added animations.
+  def delay_ms(ms)
+    @delay_ms = ms
+  end
+
+  # Iterate and yield each frame of each added animation.
+  def each_frame(previous = nil, &block)
+    previous = iterate_frames @animations, previous, &block
 
     if @loop && @loop.any?
-      loop { iterate_frames @loop, &block }
+      loop { iterate_frames @loop, previous, &block }
     end
+  end
+
+  # Run any animations added after this statement in a loop forever.
+  def forever
+    raise ArgumentError, "there's only one forever!" if @loop
+    @forever = true
+    @loop = []
   end
 
   protected
 
-  def iterate_frames(animations)
+  def iterate_frames(animations, previous)
     animations.each do |animation|
-      animation.each { |frame| yield frame }
+      animation.each_frame(previous) do |frame|
+        previous = frame
+        yield frame
+      end
     end
+    previous
   end
 end
 
-class Delayed
+# An animation with a delay between each frame.
+class WithDelay
   def initialize(ms, animation)
     @ms = ms
     @animation = animation
   end
 
-  def each
-    @animation.each do |frame|
+  def each_frame(previous)
+    @animation.each_frame(previous) do |frame|
       yield frame
       sleep @ms/1000.0
     end
   end
 end
 
+# A simple value.
 class Value
-  def initialize(color)
-    @color = color
+  def initialize(value)
+    @value = value
   end
 
-  def each
-    yield @color
+  def each_frame(previous)
+    yield "#{@value}"
   end
 end
 
+# A single timestamp
 class Timestamp
-  def each
+  def each_frame(previous)
     yield Time.now.iso8601
   end
 end
 
+# Timestamps for forever.
+class Clock
+  def each_frame(previous)
+    loop { yield Time.now.iso8601 }
+  end
+end
+
+# Runs a child animation until the flag is set.
 class Flagged
   def initialize(anim)
     @anim = anim
@@ -84,44 +112,172 @@ class Flagged
     @flag = true
   end
 
-  def each
-    @anim.each do |frame|
+  def each_frame(previous)
+    @anim.each_frame(previous) do |frame|
       break if @flag
       yield frame
     end
   end
 end
 
-bounce = Animation.new do |a|
-  0.upto(5) { |n| a << Value.new(n) }
-  4.downto(1) { |n| a << Value.new(n) }
-end
-
-main = Animation.new do |a|
-  a.delay_ms 50
-  a << bounce
-  a.delay_ms 200
-  a << bounce
-  a.delay_ms 1000
-  a.forever
-  a << Timestamp.new
-end
-
-shutdown = Animation.new do |a|
-  a.delay_ms 10
-  a << Value.new("shutting down...")
-  5.downto(1).each do |n|
-    a << Value.new(n)
+# A nice shutdown animation.
+class Shutdown
+  def each_frame(previous)
+    str = "shutting down"
+    str.length.downto(0) do |len|
+      yield str.slice(0, len)
+    end
+    yield "done."
   end
 end
 
-render = lambda { |frame| puts "frame: #{frame}" }
+# Step from the previous value (if present) to the given value.
+class FadeTo
+  def initialize(value)
+    @value = value
+  end
 
-flagged = Flagged.new(main)
-
-trap("INT") do
-  flagged.flag!
-  shutdown.each(&render)
+  def each_frame(previous)
+    previous = previous.to_i
+    list = if previous - 1 > @value
+             (previous - 1).downto @value
+           elsif previous + 1 < @value
+             (previous + 1).upto(@value)
+           else
+             []
+           end
+    list.each { |v| yield v }
+  end
 end
 
-flagged.each(&render)
+# Step from a value to another.
+class FromTo
+  def initialize(from, to)
+    @from, @to = from, to
+  end
+
+  def each_frame(previous)
+    enum = @from > @to ? @from.downto(@to) : @from.upto(@to)
+    enum.each { |v| yield v }
+  end
+end
+
+# Combine animations.
+class Combination
+
+  # Initialize a new combination of animations. Yields the new instance.
+  def initialize
+    @animations = []
+    yield self
+  end
+
+  # Add an animation to this combination.
+  def <<(animation)
+    @animations << animation
+  end
+
+  # Iterate over the combined animations, lazily evaluating each frame.
+  #
+  # If none or only one animation is provided, no combination is performed.
+  # Iteration stops if any animation returns a nil frame.
+  def each_frame(previous, &block)
+    return if @animations.empty?
+    if @animations.size == 1
+      # Pass-through, no combining needed
+      @animations.each_frame(previous, &block)
+    else
+      # Wrap each animation as a lazily-evaluated enumerator
+      enums = @animations.map do |animation|
+        Enumerator.new do |yielder|
+          animation.each_frame(previous) do |frame|
+            yielder << frame
+          end
+        end.lazy # lazy is critical!
+      end
+
+      # Then zip them together, combining each set of frames
+      enums.first.zip(*enums[1..-1]).each do |frames|
+        break if frames.any?(&:nil?)
+        block.call combine_frames(frames)
+      end
+    end
+  end
+
+  # Combine a set of frames. Override this to define your own behavior.
+  def combine_frames(frames)
+    frames.map(&:to_s).join(", ")
+  end
+end
+
+# Combine frame values by computing their sum.
+class AddFrames < Combination
+  def combine_frames(frames)
+    frames.map(&:to_i).inject(:+)
+  end
+end
+
+# ---------- Example --------- #
+
+# bounce from a previous value (default 0) to 5, then 3, then 5, then 0
+bounce = Animation.new do |a|
+  a << FadeTo.new(5)
+  a << FadeTo.new(3)
+  a << FadeTo.new(5)
+  a << FadeTo.new(0)
+end
+
+main = Animation.new do |a|
+  # Demonstrate timing changes
+  a.delay_ms 50
+  a << Value.new("faster")
+  a << bounce
+  a.delay_ms 200
+  a << Value.new("slower")
+  a << FadeTo.new(6)
+
+  # Demonstrate lazy combinations
+  a.delay_ms 1000
+  a << Value.new("lazy combiner")
+  a << Combination.new do |c|
+    c << FromTo.new(0,4) # terminate at 4 (no more values)
+    c << FromTo.new(20,30)
+    c << Clock.new # to show laziness
+  end
+
+  # Demonstrate a custom combination (summing values)
+  a.delay_ms 100
+  a << Value.new("adding frame values")
+  a << AddFrames.new do |c|
+    c << FromTo.new(0, 4)
+    c << FromTo.new(0, 4)
+  end
+
+  # Demonstrate an infinite loop
+  a << Value.new("tick tock forever")
+  a.forever
+  a.delay_ms 1000
+  a << Timestamp.new
+end
+
+# A fast shutdown animation
+shutdown = Animation.new do |a|
+  a.delay_ms 10
+  a << Value.new("caught interrupt")
+  a << Shutdown.new
+end
+
+# Wrap the main animation in an interruptable instance
+flagged = Flagged.new(main)
+
+# How to render each frame (print it out)
+render = lambda { |frame| puts "frame: #{frame}" }
+
+# Stop the animation on ctrl-c, and run a shutdown animation
+trap("INT") do
+  flagged.flag!
+  print "\r"
+  shutdown.each_frame(&render)
+end
+
+# Run the whole thing.
+flagged.each_frame(nil, &render)
