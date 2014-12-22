87e41217267983062aea326b8ffaaceda014e5e7
Nathan Witmer <nathan@zerowidth.com>
2014-12-21 21:35:23 -0700

    Make the pendulum an infinite animation
    So it doesn't reset the clock fade-in-out every frame.

--- light_show/animations/pendulum.rb
+++ light_show/animations/pendulum.rb
@@ -8,15 +8,17 @@ module LightShow
     def frames(previous)
       length = previous.length
       Enumerator.new do |y|
-        frame = LightShow.color_frame(length)
-        t = Time.now.to_f % @period / @period # constrain to 0.0-1.0
-        t = t * Math::PI * 2 # map it to 0-2pi
-        pos = (-Math.cos(t)/2 + 0.5) * length # then to 0-1 again, then length
-        left = pos.floor
-        right = (pos.floor + 1) % length # could wrap
-        frame[left]  = scaled_color(1 - (pos - pos.floor))
-        frame[right] = scaled_color(1 - (pos.ceil - pos))
-        y << frame
+        loop do
+          frame = LightShow.color_frame(length)
+          t = Time.now.to_f % @period / @period # constrain to 0.0-1.0
+          t = t * Math::PI * 2 # map it to 0-2pi
+          pos = (-Math.cos(t)/2 + 0.5) * length # then to 0-1 again, then length
+          left = pos.floor
+          right = (pos.floor + 1) % length # could wrap
+          frame[left]  = scaled_color(1 - (pos - pos.floor))
+          frame[right] = scaled_color(1 - (pos.ceil - pos))
+          y << frame
+        end
       end
     end
 
